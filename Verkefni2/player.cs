6e2ef739a016db85e389b51f807c095c319bfe5b
vallzter <valgeirtho288@nemi.tskoli.is>
2018-10-07 19:13:21 +0000

    Update player.cs

--- Verkefni2/player.cs
+++ Verkefni2/player.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D))]// breyta sem gefur til kynna að Rigidbody2D er alltaf í game objectivinu
 public class player : MonoBehaviour {
-    public float movementspeed = 10f;
-    float hreyfing = 0f;
+    public float movementspeed = 10f; //bý til movement breytuna, þarf að gera þetta svo adalpersonan fari hraðar
+    float hreyfing = 0f;// breytan fyrir hreyfingu adalpersonunar búinn til
     Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
@@ -14,14 +14,14 @@ public class player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        hreyfing = Input.GetAxis("Horizontal") * movementspeed;
+        hreyfing = Input.GetAxis("Horizontal") * movementspeed;//movement breytan, Hreyfingin er þá getAxis breytan horizontal til að hreyfa til hægri og vinstri og svo * movementSpeed svo hann fari hraðar
 
 
 	}
      
-    void FixedUpdate()
+    void FixedUpdate()//bý til fixed update útaf það er ekki hentugt að kóða movement í update
     {
-        Vector2 velocity = rb.velocity;
+        Vector2 velocity = rb.velocity;//til að finna movement
         velocity.x = hreyfing;
         rb.velocity = velocity;
 
